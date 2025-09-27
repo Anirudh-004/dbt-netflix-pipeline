f6852ef2d4d50c9346943148f315bd9a06eb1172
Anirudh Kudaragundi Anand Rao <anirudhka04@gmail.com>
2025-09-27 13:54:29 +0200

    Added result snapshots of ELT process

--- dbt/netflix/macros/no_nulls_in_columns.sql
+++ dbt/netflix/macros/no_nulls_in_columns.sql
@@ -1,7 +1,16 @@
+--{% macro no_nulls_in_columns(model) %}
+--    SELECT * FROM {{ model }} WHERE
+--    {% for col in adapter.get_columns_in_relation(model) %}
+--        {{ col.column }} IS NULL OR
+--    {% endfor %}
+--    FALSE
+--{% endmacro %}
+
 {% macro no_nulls_in_columns(model) %}
-    SELECT * FROM {{ model }} WHERE
-    {% for col in adapter.get_columns_in_relation(model) %}
-        {{ col.column }} IS NULL OR
-    {% endfor %}
-    FALSE
+select *
+from {{ model }}
+where
+  {% for col in adapter.get_columns_in_relation(model) %}
+    {{ col.name }} is null{% if not loop.last %} or{% endif %}
+  {% endfor %}
 {% endmacro %}
--- dbt/netflix/macros/relevence_score_test.sql
+++ dbt/netflix/macros/relevence_score_test.sql
@@ -5,4 +5,11 @@
 -- FROM {{ ref('fct_genome_scores') }}
 -- WHERE relevance_score <= 0
 
-{{ no_nulls_in_columns(ref('fct_genome_scores'))}}
+--{{ no_nulls_in_columns(ref('fct_genome_scores'))}}
+
+-- macros/relevence_score_test.sql
+{% test relevence_score_test(model) %}
+    select *
+    from {{ model }}
+    where relevance_score <= 0
+{% endtest %}
